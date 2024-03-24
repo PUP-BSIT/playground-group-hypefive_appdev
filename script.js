ec1fe1c5d507f4452b67e32bc7d43049d748fde1
ternalapriljoy <ternalapriljoy@gmail.com>
2024-03-24 18:33:17 +0800

    add object.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var Colors;
     Colors["GREEN"] = "green";
     Colors["YELLOW"] = "yellow";
 })(Colors || (Colors = {}));
+
 var banana = {
     name: "Banana",
     size: "small",
@@ -13,6 +14,17 @@ var banana = {
     colors: Colors.YELLOW,
     print: function () {
         console.log("The fruit is ".concat(this.name, ", it is ").concat(this.size, " in size\n        and it is ").concat(this.flavor, ", it is color ").concat(this.colors, "."));
-    }
+    },
+};
+
+var apple = {
+    name: "Apple",
+    size: "medium",
+    flavor: "crisp and tart",
+    colors: Colors.RED,
+    print: function () {
+        console.log("The fruit is ".concat(this.name, ", it is ").concat(this.size, " in size\n        and it is ").concat(this.flavor, ", it is color ").concat(this.colors, "."));
+    },
 };
 banana.print();
+apple.print();

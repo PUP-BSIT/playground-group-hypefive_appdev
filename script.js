965eff0e396f493f7eb5488ca5069d2cb546995f
Yves John B. Tapispisan <bancolitayves@gmail.com>
2024-03-24 14:43:49 +0800

    add script

---
+++ script.js
@@ -0,0 +1,18 @@
+var Colors;
+(function (Colors) {
+    Colors["RED"] = "red";
+    Colors["BLUE"] = "blue";
+    Colors["PURPLE"] = "purple";
+    Colors["GREEN"] = "green";
+    Colors["YELLOW"] = "yellow";
+})(Colors || (Colors = {}));
+var banana = {
+    name: "Banana",
+    size: "small",
+    flavor: "sweet",
+    colors: Colors.YELLOW,
+    print: function () {
+        console.log("The fruit is ".concat(this.name, ", it is ").concat(this.size, " in size\n        and it is ").concat(this.flavor, ", it is color ").concat(this.colors, "."));
+    }
+};
+banana.print();
